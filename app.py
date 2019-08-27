a1ffaa1923770d115abda71f2055ae7e0a5b9a24
Panop010 <51944962+Panop010@users.noreply.github.com>
2019-08-27 14:28:28 +0700

    Update app.py

--- app.py
+++ app.py
@@ -20,11 +20,9 @@ def callback():
     json_line = request.get_json()
     json_line = json.dumps(json_line)
     decoded = json.loads(json_line)
-    #user = decoded["events"][0]['replyToken']
-    #userText = decoded["events"][0]['message']['text']
+    user = decoded['originalDetectIntentRequest']['payload']['data']['replyToken']#user = decoded["events"][0]['replyToken']
+    userText = decoded['queryResult']['intent']['displayName']#userText = decoded["events"][0]['message']['text']
     #sendText(user,userText)
-    user = decoded['originalDetectIntentRequest']['payload']['data']['replyToken']
-    userText = decoded['queryResult']['intent']['displayName']
     if   (userText == 'สวัสดี') :
         sendText(user,'จ้าาาา')
     elif (userText == 'บ๊ายบาย') :
